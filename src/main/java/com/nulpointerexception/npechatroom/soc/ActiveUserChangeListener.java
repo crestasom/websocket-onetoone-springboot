c6f5b93c691f977ae75c090d829cd9540f3a7c70
sujan4993 <sujan4993@gmail.com>
2020-09-14 19:42:26 +0545

    initial commit

---
+++ src/main/java/com/nulpointerexception/npechatroom/soc/ActiveUserChangeListener.java
@@ -0,0 +1,7 @@
+package com.nulpointerexception.npechatroom.soc;
+
+public interface ActiveUserChangeListener {
+	
+	void notifyActiveUserChange();
+
+}
---
+++ src/main/java/com/nulpointerexception/npechatroom/soc/User.java
@@ -0,0 +1,18 @@
+package com.nulpointerexception.npechatroom.soc;
+
+import java.security.Principal;
+
+public class User implements Principal{
+
+	
+	String name;
+	 
+    public User(String name) {
+        this.name = name;
+    }
+ 
+    @Override
+    public String getName() {
+        return name;
+    }
+}
